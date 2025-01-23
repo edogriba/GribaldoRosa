7d71ee3b6aa93698413809f9d96b778807ca81cf
edogriba <76633735+edogriba@users.noreply.github.com>
2025-01-23 16:30:13 +0100

    UI enhancement (set up primary+secondary color to violet and changed many design things to make the UI nicer)

--- ITD/frontend/src/pages/Search/Student/SearchFilter.jsx
+++ ITD/frontend/src/pages/Search/Student/SearchFilter.jsx
@@ -11,11 +11,11 @@ const SearchFilter = () => {
         <div>
             <div className="flex justify-center m-5">
                 <button
-                    className="block bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                    class="block bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                     type="button"
                     onClick={toggleModal}
                 >
-                    Toggle modal
+                    Search
                 </button>
             </div>
             {isModalOpen && (
