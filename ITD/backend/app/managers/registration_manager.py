b79c26170dd60abd6ea7c1d39dc4b506662e7c5b
Thalamicks <fede.rosa2002@gmail.com>
2025-01-24 15:14:49 +0100

    initialization of RegistrationManager

---
+++ ITD/backend/app/managers/registration_manager.py
@@ -0,0 +1,9 @@
+class RegistrationManager:
+
+    def register_user(self, user_data):
+        # Implement user registration logic here
+        pass
+
+    def validate_user_data(self, user_data):
+        # Implement user data validation logic here
+        pass
