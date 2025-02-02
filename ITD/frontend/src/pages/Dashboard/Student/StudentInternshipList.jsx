4f16076838325e81981338518e7ce77b66994980
edogriba <76633735+edogriba@users.noreply.github.com>
2025-02-02 17:03:36 +0100

    Second part of bug fixing

--- ITD/frontend/src/pages/Dashboard/Student/StudentInternshipList.jsx
+++ ITD/frontend/src/pages/Dashboard/Student/StudentInternshipList.jsx
@@ -99,7 +99,7 @@ const StudentInternshipList = () => {
                         {filteredInternships.map((internship) => { return (
                                 <tr key={internship.internshipId} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                                     <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <img className="w-10 h-10 rounded-full" src={internship.company_photoPath ? `/uploads/${internship.company_photoPath}` : `/user.jpg`} alt="Profile"/>
+                                        <img className="w-10 h-10 rounded-full" src={internship.company_photoPath ? `/uploads/${internship.company_id}/${internship.company_photoPath}` : `/user.jpg`} alt="Profile"/>
                                     </th>
                                     <td className="px-6 py-4">
                                         {internship.company_name}
