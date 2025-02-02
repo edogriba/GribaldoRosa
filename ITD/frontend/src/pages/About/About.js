33bb2c5626f2db1b7658d3ec511c2a0225195a11
edogriba <76633735+edogriba@users.noreply.github.com>
2025-02-02 23:11:39 +0100

    Fixed images sizes, final commit

--- ITD/frontend/src/pages/About/About.js
+++ ITD/frontend/src/pages/About/About.js
@@ -49,62 +49,74 @@ const About = () => {
                     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 ">
                         <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
                             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our Team</h2>
-                            <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Explore the whole collection of open-source web components and elements built with the utility classes from Tailwind</p>
+                            <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">The students who made the web app possible</p>
                         </div> 
+                        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+
                         <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
                             <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-                                <span>
-                                    <img className="object-cover rounded-lg sm:rounded-none sm:rounded-l-lg" src="/edogriba.jpg" alt="Bonnie Avatar"/>
-                                </span>
-                                <div className="p-5">
-                                    <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                        <span>Edoardo Gribaldo</span>
-                                    </h3>
-                                    <span className="text-gray-500 dark:text-gray-400">Co-Founder</span>
-                                    <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Edoardo is passionate about computer science and creating this startup was a dream come true for him.</p>
-                                    <ul className="flex space-x-4 sm:mt-0">
-                                        <li>
-                                        <a href="https://github.com/edogriba">
-                                            <span className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
-                                            </span>
-                                        </a>
-                                        </li>
-                                        <li>
-                                        <a href="https://linkedin.com/edogriba">
-                                            <span className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                                                <path fillRule="evenodd" d="M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z" clipRule="evenodd" />
-                                                <path d="M7.2 8.809H4V19.5h3.2V8.809Z" />
-                                                </svg>  
-                                            </span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div> 
+                            <span>
+                                <img
+                                className="object-cover w-80 h-48 rounded-lg sm:rounded-none sm:rounded-l-lg"
+                                src="/edogriba.jpg"
+                                alt="Edoardo Gribaldo"
+                                />
+                            </span>
+                            <div className="p-5">
+                                <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                Edoardo Gribaldo
+                                </h3>
+                                <span className="text-gray-500 dark:text-gray-400">Co-Founder</span>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                Edoardo is passionate about computer science and creating this startup was a dream come true for him.
+                                </p>
+                                <ul className="flex space-x-4 sm:mt-0">
+                                <li>
+                                    <a href="https://github.com/edogriba">
+                                    <span className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                        {/* GitHub SVG icon */}
+                                    </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://linkedin.com/edogriba">
+                                    <span className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                        {/* LinkedIn SVG icon */}
+                                    </span>
+                                    </a>
+                                </li>
+                                </ul>
+                            </div>
+                            </div>
                             <div className="items-center bg-gray-50 rounded-lg shadow sm:flex dark:bg-gray-800 dark:border-gray-700">
-                                <span>
-                                    <img className="object-cover w-full rounded-lg sm:rounded-none sm:rounded-l-lg" src="/fede.jpeg" alt="Avatar" />
-                                </span>
-                                <div className="p-5">
-                                    <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                        <span>Federico Rosa</span>
-                                    </h3>
-                                    <span className="text-gray-500 dark:text-gray-400">Co-Founder</span>
-                                    <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">Federico loves Computer Science and this startup was a great chance to put to work his ideas.</p>
-                                    <ul className="flex space-x-4 sm:mt-0">
-                                        <li>
-                                            <a href="https://github.com/Thalamicks">
-                                            <span className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
-                                            </span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div> 
-                        </div>  
+                            <span>
+                                <img
+                                className="object-cover w-80 h-48 rounded-lg sm:rounded-none sm:rounded-l-lg"
+                                src="/fede.jpeg"
+                                alt="Federico Rosa"
+                                />
+                            </span>
+                            <div className="p-5">
+                                <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                Federico Rosa
+                                </h3>
+                                <span className="text-gray-500 dark:text-gray-400">Co-Founder</span>
+                                <p className="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">
+                                Federico loves Computer Science and this startup was a great chance to put to work his ideas.
+                                </p>
+                                <ul className="flex space-x-4 sm:mt-0">
+                                <li>
+                                    <a href="https://github.com/Thalamicks">
+                                    <span className="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                                        {/* GitHub SVG icon */}
+                                    </span>
+                                    </a>
+                                </li>
+                                </ul>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
                     </div>
                 </section>
                 <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
