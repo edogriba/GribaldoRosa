efbe1eb95531b0bd827700a64b7d1aad7ae8d7d2
edogriba <76633735+edogriba@users.noreply.github.com>
2025-02-02 17:51:04 +0100

    Fixed download CV button

--- ITD/frontend/src/pages/Dashboard/Student/StudentProfileCard.jsx
+++ ITD/frontend/src/pages/Dashboard/Student/StudentProfileCard.jsx
@@ -14,13 +14,13 @@ const StudentProfileCard = () => {
                     <Link to="/students/update">
                     <button className="inline-flex items-center px-4 py-2 mx-2 text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Modify</button>
                     </Link>
-                    <button 
+                    <a 
                             href={`/uploads/${user.id}/${user?.CV}`}
                             download
                             className="text-sm font-medium text-center text-white px-4 py-2 mx-2 bg-secondary-600 text-white rounded-lg hover:bg-secondary-700"
                         >
                             Download CV
-                        </button>
+                        </a>
                     
                 </div>
             </div>
